a9aff1583bccf6766b8ba76661eb65f52b95e9e7
Amogh Mannekote <msamogh@gmail.com>
2019-08-28 23:25:05 +0200

    Increase version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nonechucks",
-    version="0.3.1",
+    version="0.4.0",
     url="https://github.com/msamogh/nonechucks",
     license="MIT",
     author="Amogh Mannekote",
